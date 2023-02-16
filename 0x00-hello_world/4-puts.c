e08c64f21a38f8139f3e12d030c33e3c63a1ca38
Mohamed lachkar <Mlachkar2002@gmail.com>
2023-02-16 07:12:45 -0800

    4-puts.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a line using puts function
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
