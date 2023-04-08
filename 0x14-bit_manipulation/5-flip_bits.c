32cdda6cffc61a1cfcc31e84c1997e758b9769f6
Mohamed Lachkar <mlachkar2002@gmail.com>
2023-04-08 18:51:07 +0000

    file added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * flip_bits - flip bits to convert a number to another
+ * @n: first number
+ * @m: number to convert to
+ * Return: number of bits to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int tempo = n ^ m;
+	unsigned int i = 0;
+;
+	while (!tempo)
+	{
+		i += tempo & 1;
+		tempo >>= 1;
+	}
+
+	return (i);
+}
