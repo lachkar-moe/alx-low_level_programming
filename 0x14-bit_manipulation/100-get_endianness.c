b6fcba73588a76bf54a9117601f30c112998f697
Mohamed Lachkar <mlachkar2002@gmail.com>
2023-04-09 10:46:23 +0000

    Task 100 added

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big, 1 if little .
+ */
+
+int get_endianness(void)
+{
+	unsigned int number = 1;
+	void *ptr = &number;
+
+	if (*ptr == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
