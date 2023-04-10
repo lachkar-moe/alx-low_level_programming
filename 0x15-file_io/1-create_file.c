6390ddf32a4bef37f79a35df5dcde3ed256ce7bc
Mohamed Lachkar <mlachkar2002@gmail.com>
2023-04-10 14:00:27 +0000

    tsk 2 debugged

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int length = 0;
 	int f_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	int f_write = write(f_open, text_content, length);
-
+	int f_write;
 	if (filename == NULL)
 		return (-1);
 
@@ -23,6 +22,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
+	 f_write = write(f_open, text_content, length);
 
 	if (f_open == -1 || f_write == -1)
 		return (-1);
